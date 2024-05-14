6ff96cf8d5ab56a0fdb07d784b79a40f77d2a4ed
Imnotcoderdude <topsolo7@naver.com>
2024-05-14 16:03:12 +0900

    클라이언트에 데이터 반환방법. 제이슨

---
+++ src/main/java/com/sparta/springmvc/response/ResponseRestController.java
@@ -0,0 +1,27 @@
+package com.sparta.springmvc.response;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // 레스트 컨트롤러를 사용한 이유는 리스폰스 바디를 포함함.
+@RequestMapping("/response/rest")
+public class ResponseRestController {
+    // [Response header]
+    //   Content-Type: text/html
+    // [Response body]
+    //   {"name":"Robbie","age":95}
+    @GetMapping("/json/string")
+    public String helloStringJson() {
+        return "{\"name\":\"Robbie\",\"age\":95}";
+    }
+
+    // [Response header]
+    //   Content-Type: application/json
+    // [Response body]
+    //   {"name":"Robbie","age":95}
+    @GetMapping("/json/class")
+    public Star helloClassJson() {
+        return new Star("Robbie", 95);
+    }
+}
