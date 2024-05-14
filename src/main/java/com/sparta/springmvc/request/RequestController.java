065390617dd23f00acc5ed5b0bc6f3325e2acfa9
Imnotcoderdude <topsolo7@naver.com>
2024-05-14 19:41:13 +0900

    http 데이터를 객체로 처리하는 방법에 대한 학습

--- src/main/java/com/sparta/springmvc/request/RequestController.java
+++ src/main/java/com/sparta/springmvc/request/RequestController.java
@@ -24,7 +24,7 @@ public class RequestController {
     @GetMapping("/form/param")// @Get 방식으로 넘어오는걸 @RequestParam 으로 받는 것
     @ResponseBody
     // @RequestParam(required = false) 이렇게 required = false를 붙여서 name이 값을 받든 안받든
-    public String helloGetRequestParam(@RequestParam(required = false) String name,  int age) {
+    public String helloGetRequestParam(@RequestParam(required = false) String name, int age) {
         return String.format("Hello, @RequestParam.<br> name = %s, age = %d", name, age);
     }
 
@@ -38,6 +38,38 @@ public class RequestController {
     @ResponseBody
     public String helloPostRequestParam(@RequestParam String name, @RequestParam int age) {
         return String.format("Hello, @RequestParam.<br> name = %s, age = %d", name, age);
+    }
+
+    // [Request sample]
+    // POST http://localhost:8080/hello/request/form/model
+    // Header
+    //  Content type: application/x-www-form-urlencoded
+    // Body
+    //  name=Robbie&age=95
+    @PostMapping("/form/model")
+    @ResponseBody
+    public String helloRequestBodyForm(@ModelAttribute Star star) {
+        return String.format("Hello, @ModelAttribute.<br> (name = %s, age = %d) ", star.name, star.age);
+    }
+
+    // [Request sample]
+    // GET http://localhost:8080/hello/request/form/param/model?name=Robbie&age=95
+    @GetMapping("/form/param/model")
+    @ResponseBody
+    public String helloRequestParam(@ModelAttribute Star star) {
+        return String.format("Hello, @ModelAttribute.<br> (name = %s, age = %d) ", star.name, star.age);
+    }
+
+    // [Request sample]
+    // POST http://localhost:8080/hello/request/form/json
+    // Header
+    //  Content type: application/json
+    // Body
+    //  {"name":"Robbie","age":"95"}
+    @PostMapping("/form/json")
+    @ResponseBody
+    public String helloPostRequestJson(@RequestBody Star star) {
+        return String.format("Hello, @RequestBody.<br> (name = %s, age = %d) ", star.name, star.age);
     }
 }
 
