ad3ea1d7a057e5399d43ddea47d14ecb62032778
Imnotcoderdude <topsolo7@naver.com>
2024-05-14 19:16:37 +0900

    @PathVariable, @RequestParam 의 형태와 각 타입별 입력값 조절 학습

---
+++ src/main/java/com/sparta/springmvc/request/RequestController.java
@@ -0,0 +1,43 @@
+package com.sparta.springmvc.request;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/hello/request")
+public class RequestController {
+    @GetMapping("/form/html")
+    public String helloForm() {
+        return "hello-request-form";
+    }
+
+    // [Request sample]
+    // GET http://localhost:8080/hello/request/star/Robbie/age/95
+    @GetMapping("/star/{name}/age/{age}")// @Get 방식으로 넘어오는걸 @PathVariable 으로 받는 것
+    @ResponseBody
+    public String helloRequestPath(@PathVariable String name, @PathVariable int age) {
+        return String.format("Hello, @PathVariable.<br> name = %s, age = %d", name, age);
+    }
+
+    // [Request sample]
+    // GET http://localhost:8080/hello/request/form/param?name=Robbie&age=95
+    @GetMapping("/form/param")// @Get 방식으로 넘어오는걸 @RequestParam 으로 받는 것
+    @ResponseBody
+    // @RequestParam(required = false) 이렇게 required = false를 붙여서 name이 값을 받든 안받든
+    public String helloGetRequestParam(@RequestParam(required = false) String name,  int age) {
+        return String.format("Hello, @RequestParam.<br> name = %s, age = %d", name, age);
+    }
+
+    // [Request sample]
+    // POST http://localhost:8080/hello/request/form/param
+    // Header
+    //  Content type: application/x-www-form-urlencoded
+    // Body
+    //  name=Robbie&age=95
+    @PostMapping("/form/param") // @post 방식으로 넘어오는걸 @RequestParam 으로 받는 것
+    @ResponseBody
+    public String helloPostRequestParam(@RequestParam String name, @RequestParam int age) {
+        return String.format("Hello, @RequestParam.<br> name = %s, age = %d", name, age);
+    }
+}
+
